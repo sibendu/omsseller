82117e470fe79cef7df480e6f2a2eadbb3f48ad2
320086703 <320086703@YY193506.code1.emi.philips.com>
2020-07-02 22:57:40 +0530

    .

--- src/main/java/sd/oms/OMSController.java
+++ src/main/java/sd/oms/OMSController.java
@@ -56,24 +56,24 @@ public class OMSController {
 	public String login(@RequestParam(name = "phone", required = true) String phone, Model model,
 			HttpSession httpSession) {
 
-		System.out.println("login :: "+phone);
+		System.out.println("login :: " + phone);
 		List<Customer> customers = customerRepository.findByPhone(phone);
-		
+
 		Customer cust = null;
-		if(customers != null && customers.size() > 0) {
+		if (customers != null && customers.size() > 0) {
 			for (int i = 0; i < customers.size(); i++) {
-				if(customers.get(i).getPhone().equals(phone)) {
+				if (customers.get(i).getPhone().equals(phone)) {
 					cust = customers.get(i);
 				}
 			}
 		}
 
-		System.out.println("Customer record :: "+cust);
-		
-		if(cust != null) {
-			
+		System.out.println("Customer record :: " + cust);
+
+		if (cust != null) {
+
 			httpSession.setAttribute("customer", cust);
-			
+
 			if (cust.getType() != null && !OMSUtil.USER_TYPE_SELLER.equals(cust.getType())) {
 				model.addAttribute("message",
 						"You are not authorized to access this applicaiton. Please contact Admin.");
@@ -82,17 +82,17 @@ public class OMSController {
 
 			try {
 				ProductCategory root = catalogService.findCatalog(cust.getId()); // getJson(); Test.getDummyCatalog();
-				
+
 				String productsJson = root.toJSON();
-				
+
 				httpSession.setAttribute("products", productsJson);
-				
-				System.out.println("Catalog saved in session: "+productsJson);
-				
-			}catch (Exception e) {
+
+				System.out.println("Catalog saved in session: " + productsJson);
+
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 			try {
 				ProductCategory root = catalogService.findCatalog(cust.getId()); // getJson(); Test.getDummyCatalog();
 
@@ -202,11 +202,13 @@ public class OMSController {
 		for (int i = 0; i < items.length; i++) {
 			System.out.println(items[i].getDescription());
 		}
-
+		
+		Optional<Customer> buyer = customerRepository.findById(inputOrder.getCustomer());
+		
 		double totalPrice = 0;
 
 		Order order = new Order(null, inputOrder.getInstruction(), OMSUtil.ORDER_STATUS_NEW, null,
-				inputOrder.getCustomer(), cust.getId(), null);
+				inputOrder.getCustomer(), cust.getId(), null, buyer.get().getName());
 
 		for (ItemDTO item : items) {
 			totalPrice = totalPrice + item.getPrice() * item.getQuantity();
@@ -219,6 +221,17 @@ public class OMSController {
 
 		orderRepository.save(order);
 		System.out.println("Order created successfully :: " + order.getId());
+		
+		//Send order email - this is to seller himself/herself
+		try {
+			if(cust.getEmail() != null && cust.getEmail().indexOf("@") != -1) {	
+				catalogService.sendOrderEmail(cust.getEmail(), order, buyer.get());
+				System.out.println("Order emailed successfully.");
+			}
+		}catch(Exception e) {
+			System.out.println("Order email failure: "+e.getMessage());
+			e.printStackTrace();
+		}
 
 		model.addAttribute("clearCart", true);
 
@@ -260,27 +273,29 @@ public class OMSController {
 	@PostMapping("/processorder")
 	public String processOrder(@ModelAttribute Order inputOrder, Model model, HttpSession httpSession) {
 
-		System.out.println("Updating order: "+ inputOrder+"  "+inputOrder.getId()+"  "+inputOrder.getStatus());
+		System.out.println("Updating order: " + inputOrder + "  " + inputOrder.getId() + "  " + inputOrder.getStatus());
 
 		Optional<Order> ord = orderRepository.findById(inputOrder.getId());
-		
-		if(ord.isPresent()) {
+
+		if (ord.isPresent()) {
 			Order order = ord.get();
-			
+
 			order.setRemarks(inputOrder.getRemarks());
 			order.setStatus(inputOrder.getStatus());
-			
+
 			orderRepository.save(order);
 			System.out.println("Order updated successfully :: " + order.getId());
-			
-			model.addAttribute("order", order);
 
 			Optional<Customer> cust = customerRepository.findById(order.getCustomerId());
 			if (cust.isPresent()) {
 				model.addAttribute("customer", cust.get());
 			}
-		}else{
-			System.out.println("Order could not be found !!! id = "+inputOrder.getId());
+			
+			model.addAttribute("order", order);
+			model.addAttribute("message", "Order updated successfully.");
+			
+		} else {
+			System.out.println("Order could not be found !!! id = " + inputOrder.getId());
 			showHome(model, httpSession);
 		}
 
@@ -302,78 +317,167 @@ public class OMSController {
 
 		return "customerdetails";
 	}
-	
+
 	@GetMapping("/customer/delete/{id}")
-	public String deleteCustomer(@PathVariable(name = "id", required = true)Long id, Model model,
+	public String deleteCustomer(@PathVariable(name = "id", required = true) Long id, Model model,
 			HttpSession httpSession) {
 
 		System.out.println("deleteCustomer :: customer_id = " + id);
-		
+
 		try {
 			customerRepository.deleteById(id);
 			System.out.println("Customer record deleted successfully" + id);
 		} catch (Exception e) {
-			System.out.println("Error in deleting customer: "+id+" :: "+e.getMessage());
+			System.out.println("Error in deleting customer: " + id + " :: " + e.getMessage());
 			e.printStackTrace();
 		}
-		
+
 		model.addAttribute("customer", new Customer());
 		return "customerdetails";
 	}
-	
+
 	@GetMapping("/addcustomer")
-	public String addCustomer(Model model,HttpSession httpSession) {
+	public String addCustomer(Model model, HttpSession httpSession) {
 
 		System.out.println("Going to add new Customer");
 		model.addAttribute("customer", new Customer());
 
 		return "customerdetails";
 	}
-	
+
 	@PostMapping("/customer")
 	public String processCustomer(@ModelAttribute Customer customer, Model model, HttpSession httpSession) {
 
-		System.out.println("processCustomer :: customer_id = " + customer.getId()+"  "+model.getAttribute("operation"));
-		
-		if(customer.getId() == null) { //Adding a new customer
-			
-			Customer me = (Customer)httpSession.getAttribute("customer");
-			
+		System.out.println(
+				"processCustomer :: customer_id = " + customer.getId() + "  " + model.getAttribute("operation"));
+
+		if (customer.getId() == null) { // Adding a new customer
+
+			Customer me = (Customer) httpSession.getAttribute("customer");
+
 			customer.setSeller(me.getId());
 			customer.setCreated(new Date());
 			customer.setType(OMSUtil.USER_TYPE_CUSTOMER);
-			
+
 			customerRepository.save(customer);
-			
+
 			model.addAttribute("customer", customer);
-			
+
 			model.addAttribute("message", "New customer record created successfuly.");
-		
-		}else {
-		
-			Optional<Customer> thisCust = customerRepository.findById(customer.getId());			
+
+		} else {
+
+			Optional<Customer> thisCust = customerRepository.findById(customer.getId());
 			if (thisCust.isPresent()) {
 				System.out.println("Customer retrieved");
 				Customer thisCustomer = thisCust.get();
-				
+
 				thisCustomer.setName(customer.getName());
 				thisCustomer.setPassword(customer.getPassword());
 				thisCustomer.setPhone(customer.getPhone());
 				thisCustomer.setEmail(customer.getEmail());
 				thisCustomer.setAddress(customer.getAddress());
-				
+
 				customerRepository.save(thisCustomer);
-				
+
 				model.addAttribute("customer", thisCustomer);
 				model.addAttribute("message", "Customer record updated successfuly.");
-			}else {
-				model.addAttribute("message", "No customer record found with id# "+customer.getId()+" !!");
+			} else {
+				model.addAttribute("message", "No customer record found with id# " + customer.getId() + " !!");
 			}
 		}
-		
+
 		return "customerdetails";
 	}
+
+	@PostMapping("/item")
+	public String viewItem(@RequestParam(name = "itemId", required = true) Long itemId,  Model model,
+			HttpSession httpSession) {
+		
+		System.out.println("viewItem :: item_id = " + itemId);
+		
+		if(itemId != null || itemId.longValue() != 0) {
+			SKUItem item = catalogService.findItem(itemId);
+			catalogService.saveItem(item);
+			model.addAttribute("item", item);
+		}else {
+			model.addAttribute("message", "Please contact your admin for adding items.");
+		}
+		return "item";
+	}
 	
+	@PostMapping("/item/save")
+	public String saveItem(@ModelAttribute SKUItem formItem, Model model,
+			HttpSession httpSession) {
+		
+		Long itemId = formItem.getId();
+		System.out.println("saveItem :: item_id = " + itemId);
+		
+		if(itemId != null || itemId.longValue() != 0) {
+			SKUItem item = catalogService.findItem(itemId);
+			
+			item.setName(formItem.getName());
+			item.setDescription(formItem.getDescription());
+			item.setPrice(formItem.getPrice());
+			item.setMin(formItem.getMin());
+			item.setMax(formItem.getMax());
+			item.setStep(formItem.getStep());
+			item.setDefaultValue(formItem.getDefaultValue());
+			item.setUnit(formItem.getUnit());		
+			
+			catalogService.saveItem(item);
+			
+			model.addAttribute("item", item);
+			
+			//Update product catalog saved in session
+			try {
+				Customer cust = (Customer)httpSession.getAttribute("customer");
+				ProductCategory root = catalogService.findCatalog(cust.getId());
+				String productsJson = root.toJSON();
+				httpSession.setAttribute("products", productsJson);
+				System.out.println("Catalog updated in session");
+				
+				model.addAttribute("message", "Item updated successfully.");
+				
+			} catch (Exception e) {
+				model.addAttribute("Item updated successfully, but product catalog could not be updated. Please log in again.");
+			}				
+			
+		}else {
+			model.addAttribute("message", "Please contact your admin for adding items.");
+		}
+		return "item";
+	}
+
+	@PostMapping("/item/delete")
+	public String deleteItem(@ModelAttribute SKUItem skuItem, Model model,
+			HttpSession httpSession) {
+
+		Long itemId = skuItem.getId();
+		System.out.println("deleteItem :: item_id = " + itemId);
+
+		catalogService.removeItem(skuItem.getId());
+		System.out.println("Item deleted. id = "+skuItem.getId());
+
+		model.addAttribute("item", new SKUItem());
+		
+		//Update product catalog saved in session
+		try {
+			Customer cust = (Customer)httpSession.getAttribute("customer");
+			ProductCategory root = catalogService.findCatalog(cust.getId());
+			String productsJson = root.toJSON();
+			httpSession.setAttribute("products", productsJson);
+			System.out.println("Catalog updated in session");
+			
+			model.addAttribute("message", "Item removed successfully.");
+			
+		} catch (Exception e) {
+			model.addAttribute("Item removed successfully, but product catalog could not be updated. Please log in again.");
+		}	
+	
+		return "item";
+	}
+
 	/*
 	 * @GetMapping("/order/delete/{id}") public String deleteOrder(
 	 * 
--- src/main/java/sd/oms/util/OMSUtil.java
+++ src/main/java/sd/oms/util/OMSUtil.java
@@ -1,5 +1,9 @@
 package sd.oms.util;
 
+import java.util.Properties;
+ 
+import sd.oms.model.Order;
+
 public class OMSUtil {
 	
 	public static String USER_TYPE_SELLER = "SELLER";
@@ -16,4 +20,11 @@ public class OMSUtil {
 	public static String ITEM_STATUS_ORDERED = "Ordered";
 	public static String ITEM_STATUS_CANCELLED = "Cancelled";
 	public static String ITEM_STATUS_NOT_AVAILABLE = "Not Available";
+	
+	public static void main(String[] args) {
+
+		// Recipient's email ID needs to be mentioned.
+        String to = "fromaddress@gmail.com";
+		
+	}
 }
